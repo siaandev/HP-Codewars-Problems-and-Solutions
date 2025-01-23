ce6a9b970475af8ea116c941cba461d220553820
siaan <siaandev@gmail.com>
2025-01-23 17:01:57 +0100

    Update 1.py

--- Solutions/Barcelona/1.py
+++ Solutions/Barcelona/1.py
@@ -1,4 +1,4 @@
 # Problem 1: Let's begin introducing ourselves.
 
-name = input(str(""))
+name = input(str("")) # we ask the user for the name of the group
 print("Greetings, " + name, "the Great! I genuflect in your general direction!")
