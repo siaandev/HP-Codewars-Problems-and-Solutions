8a5702896a1caff5dc751b2aff0b1a01b63eac5b
claudia rodriguez del rio <siaandev@gmail.com>
2025-01-23 17:00:28 +0100

    Add Solutions folder

---
+++ Solutions/Barcelona/0.py
@@ -0,0 +1,4 @@
+# Problem 0: We are at HP!
+
+group = "3"  # variable type: str
+print("Hello world, we are at HP. We are group " + group)
---
+++ Solutions/Barcelona/1.py
@@ -0,0 +1,4 @@
+# Problem 1: Let's begin introducing ourselves.
+
+name = input(str(""))
+print("Greetings, " + name, "the Great! I genuflect in your general direction!")
